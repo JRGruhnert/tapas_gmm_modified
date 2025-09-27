d1e05dce55b436d843cbbbdfe8e74329301d810d
Jan Gruhnert <36522183+JRGruhnert@users.noreply.github.com>
2025-09-27 17:28:57 +0200

    new branch

--- conf/master/shared/agent.py
+++
@@ -1,3 +0,0 @@
-from tapas_gmm.master_project.agent import AgentConfig
-
-debug = AgentConfig(batch_size=8, mini_batch_size=4)
--- conf/master/shared/collect_env.py
+++
@@ -1,8 +0,0 @@
-from tapas_gmm.master_project.environment import MasterEnvConfig
-from conf.env.calvin.env_collect_conf import calvin_env_config
-
-
-collect_env = MasterEnvConfig(
-    calvin_config=calvin_env_config,
-    debug_vis=True,
-)
